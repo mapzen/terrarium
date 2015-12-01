138abf9d00d92fae56c4c118f8210876c17d3467
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-12-01 18:35:59 -0500

    adding lower zoom levels

--- src/makeTiles.py
+++ src/makeTiles.py
@@ -5,7 +5,7 @@ import sys
 from tools import makeTile, makeTiles
 
 osm_id = "111968" #sys.argv[1]
-zooms = "0-17" #sys.argv[2]
+zooms = "3-17" #sys.argv[2]
 
 makeTiles(osm_id, zooms)
 # makeTile(655,1582,12)
