cb268813f0bc7742d6d216bda5f14028c605acc8
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-12-15 16:37:41 -0500

    3-20

--- data/makeBTiles.py
+++ data/makeBTiles.py
@@ -10,18 +10,18 @@ from common import getStringRangeToArray
 
 DATA_PATH = '../data/B'
 OSM_ID = "111968" 
-ZOOMS = "3-17"
+ZOOMS = "3-20"
 
 if (len(sys.argv) > 1):
 	ID = sys.argv[1]
 if (len(sys.argv) > 2):
 	ZOOMS = sys.argv[2]
 
-makeTile(DATA_PATH, 655, 1582, 12, True)
-makeTile(DATA_PATH, 1310, 3166, 13, True)
+# makeTile(DATA_PATH, 655, 1582, 12, True)
+# makeTile(DATA_PATH, 1310, 3166, 13, True)
 
-# points = getPointsOfID(OSM_ID)
-# zoom_array = getStringRangeToArray(ZOOMS)
+points = getPointsOfID(OSM_ID)
+zoom_array = getStringRangeToArray(ZOOMS)
 
-# for zoom in zoom_array:
-#     makeTilesOfPoints(DATA_PATH, points, zoom, True)
+for zoom in zoom_array:
+    makeTilesOfPoints(DATA_PATH, points, zoom, True)
