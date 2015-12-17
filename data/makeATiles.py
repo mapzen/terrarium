cb5242f07e411d2e7dd7f656f90206f11e2149ac
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-12-17 15:34:57 -0500

    seting dir A

--- data/makeATiles.py
+++ data/makeATiles.py
@@ -22,6 +22,8 @@ if (len(sys.argv) > 1):
 	ID = sys.argv[1]
 if (len(sys.argv) > 2):
 	ZOOMS = sys.argv[2]
+if (len(sys.argv) > 3):
+	DATA_PATH = sys.argv[3]
 
 # Create A tile directory
 os.system("mkdir " + DATA_PATH)
